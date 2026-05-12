7f25c7111df9a10cdb63a7580fd26aeb4f1816e7
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2026-05-12 20:10:50 +0200

    Github Pages ding

--- portfolio-website-van-berkan/src/App.jsx
+++ portfolio-website-van-berkan/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
 
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio-van-berkan">
         <Navigationbar />
         <Routes>
           <Route path="/" element={<Homepagina />} />
